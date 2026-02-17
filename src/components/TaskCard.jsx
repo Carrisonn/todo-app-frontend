d0ab65a1a56ea8da373910ab4680e9a857842953
Carrison <carlos97181034535@gmail.com>
2026-02-17 18:15:34 +0100

    Refactor: Update TaskCard component to use useTaskAPI for task actions and improve message handling

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,16 +1,17 @@
-import styles from './TaskCard.module.css'
-import { useTaskStore } from '../store/taskStore.js'
+import { useStore } from '../store/store.js'
+import { useTaskAPI } from '../hooks/useTaskAPI.jsx'
 import { Toast } from './Toast.jsx'
+import styles from './TaskCard.module.css'
 
 export function TaskCard({ task }) {
-  const deleteTask = useTaskStore(state => state.deleteTask)
-  const toogleTaskStatus = useTaskStore(state => state.toogleTaskStatus)
-  const setEditingTask = useTaskStore(state => state.setEditingTask)
-  const message = useTaskStore(state => state.message)
+  const setEditingTask = useStore(state => state.setEditingTask)
+  const message = useStore(state => state.message)
 
-  const handleDeleteTask = taskID => {
-    if (!taskID) return
-    deleteTask(taskID)
+  const { deleteTask, toogleTaskStatus } = useTaskAPI()
+
+  const handleDeleteTask = id => {
+    if (!id) return
+    deleteTask(id)
   }
 
   const handleEditingTask = task => {
@@ -19,29 +20,31 @@ export function TaskCard({ task }) {
   }
 
   return (
-    <div className={styles.task_card}>
-      <p className={styles.task_name}><span>{task.task}</span></p>
-      <p className={styles.task_priority}>Prioridad: <span>{task.priority}</span></p>
-      <div className={styles.state_wrapper}>
-        <label htmlFor="checkbox">
-          <p>Estado: <span>{task.status}</span></p>
-          <input
-            checked={task?.status === 'Completada'}
-            onChange={() => toogleTaskStatus(task.id, task.status)}
-            type="checkbox"
-            id='checkbox'
-          />
-        </label>
-      </div>
-      <div className={styles.buttons_wrapper}>
-        <button className={styles.delete_button} onClick={() => handleDeleteTask(task.id)}>Borrar</button>
-        <button className={styles.edit_button} onClick={() => handleEditingTask(task)}>Editar</button>
-      </div>
-      <p className={styles.creation_date}>{task.creation_date}</p>
+    <>
+      {message && <Toast message={message} />}
 
-      {
-        message && <Toast message={message} />
-      }
-    </div>
+      <div className={styles.task_card}>
+        <p className={styles.task_name}><span>{task.task}</span></p>
+        <p className={styles.task_priority}>Prioridad: <span>{task.priority}</span></p>
+
+        <div className={styles.state_wrapper}>
+          <label htmlFor="checkbox">
+            <p>Estado: <span>{task.status}</span></p>
+            <input
+              checked={task?.status === 'Completada'}
+              onChange={() => toogleTaskStatus(task.id, task.status)}
+              type="checkbox"
+              id='checkbox'
+            />
+          </label>
+        </div>
+
+        <div className={styles.buttons_wrapper}>
+          <button className={styles.delete_button} onClick={() => handleDeleteTask(task.id)}>Borrar</button>
+          <button className={styles.edit_button} onClick={() => handleEditingTask(task)}>Editar</button>
+        </div>
+        <p className={styles.creation_date}>{task.creation_date}</p>
+      </div>
+    </>
   )
 }
