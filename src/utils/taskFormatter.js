52c32376c63ba609006844cd2454bc969575264a
Carrison <carlos97181034535@gmail.com>
2026-02-17 18:17:01 +0100

    Refactor: Update taskFormatter function to include editingTask status and improve variable naming

--- src/utils/taskFormatter.js
+++ src/utils/taskFormatter.js
@@ -1,16 +1,18 @@
-export function taskFormatter(tasks, priority) {
-  const task = tasks.replace(/^\w/, character => character.toLocaleUpperCase())
+export function taskFormatter(task, priority, editingTask) {
+  const taskFormatted = task.replace(/^\w/, character => character.toLocaleUpperCase())
+  const status = editingTask ? editingTask.status : 'Pendiente'
+
   const date = new Date()
   const day = String(date.getDate()).padStart(2, '0')
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const year = date.getFullYear()
   const creationTime = date.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', hour12: false })
-  const creation_date = `${day}/${month}/${year} - ${creationTime}`
+  const creationDate = `${day}/${month}/${year} - ${creationTime}`
 
   return {
-    task,
+    task: taskFormatted,
+    status,
     priority,
-    status: 'Pendiente',
-    creation_date
+    creation_date: creationDate
   }
 }
