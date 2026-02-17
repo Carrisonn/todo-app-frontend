9a61eceb8024a1b14d0dc845be2f949c831a2ece
Carrison <carlos97181034535@gmail.com>
2026-02-17 18:15:24 +0100

    Refactor: Simplify Form component by integrating useForm hook and removing redundant state management

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,60 +1,26 @@
-import { useEffect, useId, useState } from 'react'
-import { useTaskStore } from '../store/taskStore.js'
-import { taskFormatter } from '../utils/taskFormatter.js'
+import { useForm } from '../hooks/useForm.jsx'
 import { Toast } from './Toast.jsx'
 import styles from './Form.module.css'
 
 export function Form() {
-  const idInput = useId()
-  const idSelect = useId()
-
-  const [inputValue, setInputValue] = useState('')
-  const [selectValue, setSelectValue] = useState('')
-
-  const createTask = useTaskStore(state => state.createTask)
-  const message = useTaskStore(state => state.message)
-  const editTask = useTaskStore(state => state.editTask)
-  const editingTask = useTaskStore(state => state.editingTask)
-  const setEditingTask = useTaskStore(state => state.setEditingTask)
-
-  useEffect(() => {
-    if (editingTask) {
-      setInputValue(editingTask.task)
-      setSelectValue(editingTask.priority)
-    }
-  }, [editingTask])
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const task = formData.get(idInput).trim()
-    const priority = formData.get(idSelect)
-    if (!task || !priority) return
-
-    const data = taskFormatter(task, priority)
-    editingTask ? editTask(editingTask.id, data) : createTask(data)
-
-    setEditingTask(null)
-    setInputValue('')
-    setSelectValue('')
-  }
-
-  const handleInputChange = event => {
-    setInputValue(event.target.value)
-  }
-
-  const handleSelectChange = event => {
-    setSelectValue(event.target.value)
-  }
-
-  const buttonText = editingTask ? 'Guardar cambios' : 'Agregar tarea'
+  const {
+    inputValue,
+    selectValue,
+    message,
+    handleSubmit,
+    handleInputChange,
+    handleSelectChange,
+    buttonText
+  } = useForm()
 
   return (
     <section >
+      {message && <Toast message={message} />}
+
       <form className={styles.form} onSubmit={handleSubmit} method='POST'>
         <input
           className={styles.input}
-          name={idInput}
+          name='task'
           value={inputValue}
           onChange={handleInputChange}
           type="text"
@@ -62,17 +28,14 @@ export function Form() {
           autoComplete='off'
           required
         />
-        <select className={styles.select} onChange={handleSelectChange} value={selectValue} name={idSelect} required>
+
+        <select className={styles.select} onChange={handleSelectChange} value={selectValue} name='priority' required>
           <option value="" defaultChecked hidden>Selecciona una prioridad</option>
           <option value="Baja">Baja</option>
           <option value="Media">Media</option>
           <option value="Alta">Alta</option>
         </select>
         <button className={styles.button} type='submit'>{buttonText}</button>
-
-        {
-          message && <Toast message={message} />
-        }
       </form>
     </section>
   )
